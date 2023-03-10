f13c8b0f5052192a4b87fd4223550b84a50789de
yeonblue <yeondublue@naver.com>
2023-03-10 21:00:44 +0900

    first commit

---
+++ TipCaculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCaculator
+//
+//  Created by yeonBlue on 2023/03/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
