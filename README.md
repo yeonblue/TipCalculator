955a2efa8572c55964c338887b0c57e4be9438b6
yeonblue <yeondublue@naver.com>
2023-03-10 20:56:39 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# TipCalculator
+- UIKit with MVVM, Combine, SnapKit, Snapshot/UI/Unit Tests
+- practice Advanced SwiftUI topics
+- Coding views programatically with SnapKit (without Storyboards)
+- etc ..
---
+++ TipCaculator/TipCaculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCaculatorApp.swift
+//  TipCaculator
+//
+//  Created by yeonBlue on 2023/03/10.
+//
+
+import SwiftUI
+
+@main
+struct TipCaculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
